83b244f04fd6d1c13e2b96a8b976cb7bc781b878
anatoly <tolitius@gmail.com>
2014-11-06 01:43:34 -0500

    moving to [zeromq 4.0.5], [clojure 1.6.0], [netty 3.9.5] + other newer dependencies... [done]. switch to netyy 4.0+... [next]

--- project.clj
+++ project.clj
@@ -4,14 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src" "src/highlander"]
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [com.taoensso/carmine "1.6.0"]
-                 [io.netty/netty "3.6.3.Final"]
-                 [clj-time "0.4.5"]
-                 [lasync "0.1.0"]
-                 [org.clojure/tools.cli "0.2.2"]
-                 [zmq/zmq "3.2.2"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [com.taoensso/carmine "2.7.1"]
+                 ;; [io.netty/netty-all "4.0.24.Final"]
+                 [io.netty/netty "3.9.5.Final"]
+                 [clj-time "0.8.0"]
+                 [lasync "0.1.1"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [zmq/zmq "4.0.5"]]
   
   :java-source-paths ["src/bench" "src/lmax"]
   :native-path "/usr/local/lib"
